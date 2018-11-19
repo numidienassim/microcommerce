e8401f5aadbb3bfbe9dc098ba276246c210c759b
Nassim Chelli <Nassim.Chelli@keyruscorp.key>
2018-11-20 00:16:42 +0100

    First commit

---
+++ src/main/java/com/ecommerce/microcommerce/controller/ProductController.java
@@ -0,0 +1,20 @@
+package com.ecommerce.microcommerce.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+	@RequestMapping(value="/productList",method=RequestMethod.GET)
+	public String getProducts() {
+		return "Product list";
+	}
+	
+	@RequestMapping(value = "/Product/{id}", method = RequestMethod.GET)
+	public String getProduct(@PathVariable int id) {
+		return "You are askink for product with id=  " + id;
+	}
+}
